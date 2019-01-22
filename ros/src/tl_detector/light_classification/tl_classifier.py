42aef8d6d18b870e01fa268b5aaf45200a516d98
elgeweily <mohamed.elgeweily@gmail.com>
2019-01-22 20:19:27 +0400

    code moved to init

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -7,25 +7,23 @@ from helpers import load_graph
 
 import os.path as path
 
-# Load frozen graph of trained model
+# Model path
 five_up = path.abspath(path.join(__file__, '../../../../..'))
 SSD_INCEPTION_SIM = path.join(five_up, 'models/ssd_inception_v2_alex_sim/frozen_inference_graph.pb')
-detection_graph = load_graph(SSD_INCEPTION_SIM)
-
-# Get tensors
-# Input placeholder for the image
-image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-# Each box represents a part of the image where a particular object was detected
-detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-# Each score represent the level of confidence for each of the objects
-detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-# The classification of the object (integer id)
-detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 
 class TLClassifier(object):
     def __init__(self):
-	tf.reset_default_graph()
-	self.sess = tf.Session(graph=detection_graph)
+	# Load frozen graph of trained model
+	self.detection_graph = load_graph(SSD_INCEPTION_SIM)
+
+	# Get tensors
+	self.image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+	self.detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+	self.detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+	self.detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+	# Create session
+	self.sess = tf.Session(graph=self.detection_graph)
 
     def simple_opencv_red_color_classifier(self,image):
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -59,17 +57,17 @@ class TLClassifier(object):
         image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
                      
         # Actual detection
-        (boxes, scores, classes) = self.sess.run([detection_boxes, detection_scores, detection_classes], feed_dict={image_tensor: image_np})
+        (boxes, scores, classes) = self.sess.run([self.detection_boxes, self.detection_scores, self.detection_classes], feed_dict={self.image_tensor: image_np})
 
         # Remove unnecessary dimensions
         scores = np.squeeze(scores)
         classes = np.squeeze(classes)
         
         for i in range(len(classes)):
-		print('class=', classes[i])
-		print('score=', scores[i])
-            	if classes[i] == 2 and scores[i] > 0.5: # if red light with confidence more than 50%
-                	return TrafficLight.RED
+	    print('class =', classes[i])
+	    print('score =', scores[i])
+            if classes[i] == 2 and scores[i] > 0.5: # if red light with confidence more than 50%
+                return TrafficLight.RED
 
         return TrafficLight.UNKNOWN
     
